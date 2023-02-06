1c9e9ea3096035cf780f7fafb3d16c6ccb00ae00
Marco <incardonamarco0@gmail.com>
2023-02-06 15:27:15 +0100

    useeffect 4

--- useeffect-04/src/GithubUserList.js
+++ useeffect-04/src/GithubUserList.js
@@ -3,7 +3,7 @@ import { GitHubUser2 } from "./GithubUser"
 
 export function GitHubUserList({usernames}){
 
-    const list = usernames.forEach().map((name) => <li><GitHubUser2 username={name}/></li>)
+    const list = usernames.map((name) => <li><GitHubUser2 username={name}/></li>)
     return(
         <ul>
             {list}
