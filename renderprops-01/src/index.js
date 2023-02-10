6d901489d4b134a0a66b87dfd9f90fc0db639e57
Marco <incardonamarco0@gmail.com>
2023-02-10 13:58:42 +0100

    fine esercizi

--- renderprops-01/src/index.js
+++ renderprops-01/src/index.js
@@ -1,12 +1,17 @@
-import { ToDo } from "./ToDo"
+import TodoList from "./ToDoList"
 import ReactDOM from "react-dom"
 import React from "react"
 
 const root = document.querySelector("#root")
 
-ReactDOM.render(<ToDo 
-render={(items) =>{
-    items.forEach(() =>{
-        items
-    })
-}}/>, root)
+ReactDOM.render(<TodoList render={(items, handleDeleteTodo) => {
+    console.log(items);
+    return (
+    <ul>
+      {items.map((item) => {
+          return (
+                  <li key={item.key}>{item.description} <span>{item.done ? "Completato" : ""}</span> <button id={item.key} onClick={handleDeleteTodo}>Delete Todo</button></li>
+          )
+      })}
+  </ul>)
+  }}></TodoList>, root)
